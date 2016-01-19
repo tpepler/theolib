72b9d1a4d8820be8661336c37e182cabfb7e411b
Theo Pepler <theo.pepler@glasgow.ac.uk>
2016-01-19 16:26:32 +0000

    Fix ts.stack function

--- R/ts_stack.R
+++ R/ts_stack.R
@@ -1,7 +1,7 @@
-ts.stack <- function(x, window=floor(length(x)/4)){
+ts.stack <- function(x, window=floor(length(x)/2)){
   ts.mat<-NULL
   for(i in 1:(length(x)-window+1)){
-    ts.mat<-rbind(ts.mat,x[i:(i+window-1)])
+    ts.mat<-cbind(ts.mat,x[i:(i+window-1)])
   }
   return(ts.mat)
 }
