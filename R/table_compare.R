0732a6f27de2009edc02cc377ceb7f721314dbf0
Theo Pepler <theo.pepler@glasgow.ac.uk>
2018-11-07 11:43:49 +0000

    Add contingency table function

---
+++ R/table_compare.R
@@ -0,0 +1,14 @@
+# Function to 2x2 contingency table and calculate associated statistics
+table_compare <- function(pred, actual){
+  outlist <- vector('list', 6)
+  names(outlist) <- c('cont.table', 'sens', 'spec', 'misclass.rate', 'false.pos', 'false.neg')
+  ind <- !(is.na(pred) | is.na(actual)) # indicate pairs where both values are non-missing
+  (.Table <- table(pred = pred[ind], actual = actual[ind]))
+  outlist$cont.table <- .Table
+  outlist$sens <- .Table[2, 2] / sum(.Table[, 2]) # sensitivity
+  outlist$spec <- .Table[1, 1] / sum(.Table[, 1]) # specificity
+  outlist$misclass.rate <- (.Table[1, 2] + .Table[2, 1]) / sum(.Table) # misclassification rate
+  outlist$false.pos <- .Table[2, 1] / sum(.Table[, 1]) # false positive rate
+  outlist$false.neg <- .Table[1, 2] / sum(.Table[, 2]) # false negative rate
+  return(outlist)
+}
